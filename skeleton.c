aa91f77c713f25d5bccf3cc8b777c2f2276036ad
Sat Garcia <sat@cs.ucsd.edu>
2010-03-24 20:33:10 +0000

    counter no longer static (limitation of inliner currently), fixed args for a couple funcs

--- skeleton.c
+++ skeleton.c
@@ -9,7 +9,7 @@
 #define TRUE 1
 #define FALSE 0
 
-static unsigned long long int counter = 0;
+unsigned long long int counter = 0;
 
 // basic stuff needed
 void addInit(unsigned int new_init) {counter++;}
@@ -28,7 +28,8 @@ void linkInit(const void* cond) {counter++;}
 void linkInitToCondition(const void* rhs, const void* lhs) {counter++;}
 
 void logRegionEntry(unsigned int region_id, unsigned int region_type) {counter++;}
-void logRegionExit() {counter++;}
+void logRegionExit(unsigned int region_id, unsigned int region_type) {counter++;}
+
 
 unsigned int logBinaryOp(unsigned int id, unsigned int bb_id, int opcode, const void* arg1, const void* arg2, const void* address) {counter++;}
 unsigned int logBinaryOpConst(unsigned int id, unsigned int bb_id, int opcode, const void* arg, const void* address) {counter++;}
@@ -54,7 +55,7 @@ void stackVariableDealloc(unsigned int bb_id, const void* address) {counter++;}
 void linkAddrToArgName(unsigned int bb_id, const void* address, char* argname) {counter++;}
 void createArgLink(const void* address) {counter++;}
 void linkArgToAddr(unsigned int bb_id, const void* address) {counter++;}
-void linkArgToConst(unsigned int bb_id) {counter++;}
+void linkArgToConst() {counter++;}
 void transferAndUnlinkArg(unsigned int bb_id, unsigned int id, const void* a) {counter++;}
 void transferAndUnlinkArgName(unsigned int bb_id, const void* a, char* argname) {counter++;}
 
@@ -62,7 +63,7 @@ void addReturnValueLink(void* address) {counter++;}
 
 void onReturn(unsigned int op_id, unsigned int bb_id, const void* ret_val_addr) {counter++;}
 
-void setReturnTimestampConst(unsigned int bb_id) {counter++;}
+void setReturnTimestampConst() {counter++;}
 void setReturnTimestamp(unsigned int bb_id, const void* retval) {counter++;}
 void getReturnTimestamp(unsigned int id, unsigned int bb_id, const void* address) {counter++;}
 
