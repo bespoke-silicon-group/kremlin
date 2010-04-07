9d37367026cfbb4e732d2cf3bdf870ddf566fd69
Donghwan Jeon <djeon@cs.ucsd.edu>
2010-04-07 04:52:02 +0000

    several logXXX functions are implemented

--- defs.h
+++ defs.h
@@ -15,7 +15,7 @@
 
 typedef unsigned long       UInt32;
 typedef signed long         Int32;
-typedef unsigned int        UInt;
+typedef unsigned int		UInt;
 typedef signed int          Int;
 typedef unsigned long long  UInt64;
 typedef signed long long    Int64;
@@ -74,41 +74,41 @@ UInt64 getTimestamp(TEntry* entry, UInt32 level, UInt32 version);
 void updateTimestamp(TEntry* entry, UInt32 level, UInt32 version, UInt64 timestamp);
 
 /* The following funcs are inserted by the critical path instrumentation pass */
-void* logBinaryOp(int op_cost, unsigned int src0, unsigned int src1, unsigned int dest); 
-void* logBinaryOpConst(int op_cost, unsigned int src, unsigned int dest); 
+void* logBinaryOp(UInt opCost, UInt src0, UInt src1, UInt dest); 
+void* logBinaryOpConst(UInt opCost, UInt src, UInt dest); 
 
-void* logAssignment(unsigned int src, unsigned int dest);
-void* logAssignmentConst(unsigned int dest); 
+void* logAssignment(UInt src, UInt dest);
+void* logAssignmentConst(UInt dest); 
 
-void* logInsertValue(unsigned int src_addr, unsigned int dst_addr); 
-void* logInsertValueConst(unsigned int dst_addr); 
+void* logInsertValue(UInt src_addr, UInt dst_addr); 
+void* logInsertValueConst(UInt dst_addr); 
 
-void* logLoadInst(const void* src_addr, unsigned int dest); 
-void* logStoreInst(unsigned int src, const void* dest_addr); 
+void* logLoadInst(const void* src_addr, UInt dest); 
+void* logStoreInst(UInt src, const void* dest_addr); 
 
-void logPhiNode(unsigned int dest, unsigned int num_incoming_values, unsigned int num_t_inits, ...); 
+void logPhiNode(UInt dest, UInt num_incoming_values, UInt num_t_inits, ...); 
 
-void addControlDep(unsigned int cond);
+void addControlDep(UInt cond);
 void removeControlDep();
 
-void addReturnValueLink(unsigned int dest);
-void logFuncReturn(unsigned int src); 
+void addReturnValueLink(UInt dest);
+void logFuncReturn(UInt src); 
 
-void linkArgToLocal(unsigned int src); 
+void linkArgToLocal(UInt src); 
 void linkArgToConst();
-void transferAndUnlinkArg(unsigned int dest); 
+void transferAndUnlinkArg(UInt dest); 
 
-unsigned int logLibraryCall(unsigned int cost, unsigned int dest, unsigned int num_in, ...); 
+UInt logLibraryCall(UInt cost, UInt dest, UInt num_in, ...); 
 
-void logBBVisit(unsigned int bb_id); 
+void logBBVisit(UInt bb_id); 
 
 
 /* The following functions are inserted by region instrumentation pass */
 void initProfiler();
 void deinitProfiler();
 
-void logRegionEntry(unsigned int region_id, unsigned int region_type);
-void logRegionExit(unsigned int region_id, unsigned int region_type);
+void logRegionEntry(UInt region_id, UInt region_type);
+void logRegionExit(UInt region_id, UInt region_type);
 
 
 
@@ -117,26 +117,26 @@ void logRegionExit(unsigned int region_id, unsigned int region_type);
 
 /* The following functions are deprecated. Don't bother implementing them */
 void linkInitToCondition(const void* rhs, const void* lhs);
-unsigned int logInductionVarDependence(const void* induct_var); 
+UInt logInductionVarDependence(const void* induct_var); 
 
-unsigned int logOutputToConsole(unsigned int id, unsigned int bb_id, int num_out, ...); 
-unsigned int logInputFromConsole(unsigned int id, unsigned int bb_id, int num_in, ...); 
+UInt logOutputToConsole(UInt id, UInt bb_id, int num_out, ...); 
+UInt logInputFromConsole(UInt id, UInt bb_id, int num_in, ...); 
 
-void stackVariableAlloc(unsigned int bb_id, const void* address); 
-void stackVariableDealloc(unsigned int bb_id, const void* address); 
+void stackVariableAlloc(UInt bb_id, const void* address); 
+void stackVariableDealloc(UInt bb_id, const void* address); 
 
-void linkAddrToArgName(unsigned int bb_id, const void* address, char* argname); 
+void linkAddrToArgName(UInt bb_id, const void* address, char* argname); 
 
 void setReturnTimestampConst();
-void setReturnTimestamp(unsigned int bb_id, const void* retval);
-void getReturnTimestamp(unsigned int id, unsigned int bb_id, const void* address);
+void setReturnTimestamp(UInt bb_id, const void* retval);
+void getReturnTimestamp(UInt id, UInt bb_id, const void* address);
 
-void onBasicBlockEntry(unsigned int bb_id);
+void onBasicBlockEntry(UInt bb_id);
 
 void openOutputFile();
 void closeOutputFile();
 
-void linkInit(unsigned int cond);
+void linkInit(UInt cond);
 void removeInit();
 
 void printProfileData(void);
--- skeleton.c
+++ skeleton.c
@@ -11,3 +11,148 @@
 
 int foo() {
 }
+
+int getRegionLevel() {
+	return 5;
+}
+
+UInt64 getCdt(int level) {
+	return 0x1000;
+}
+
+UInt getVersion() {
+	static UInt version = 0;
+	return version++;
+}
+
+void* logBinaryOp(UInt opCost, UInt src0, UInt src1, UInt dest) {
+	int level = getRegionLevel();
+	UInt version = getVersion();
+	int i = 0;
+	TEntry* entry0 = getLTEntry(src0);
+	TEntry* entry1 = getLTEntry(src1);
+	TEntry* entryDest = getLTEntry(dest);
+	
+	for (i = 0; i < level; i++) {
+		UInt64 cdt = getCdt(i);
+		UInt64 ts0 = getTimestamp(entry0, i, version);
+		UInt64 ts1 = getTimestamp(entry1, i, version);
+		UInt64 greater0 = (ts0 > ts1) ? ts0 : ts1;
+		UInt64 greater1 = (cdt > greater0) ? cdt : greater0;
+		updateTimestamp(entryDest, i, version, greater1 + opCost);
+	}
+
+	return NULL;
+}
+
+
+void* logBinaryOpConst(UInt opCost, UInt src, UInt dest) {
+	int level = getRegionLevel();
+	UInt version = getVersion();
+	int i = 0;
+	TEntry* entry0 = getLTEntry(src);
+	TEntry* entryDest = getLTEntry(dest);
+	
+	for (i = 0; i < level; i++) {
+		UInt64 cdt = getCdt(i);
+		UInt64 ts0 = getTimestamp(entry0, i, version);
+		UInt64 greater1 = (cdt > ts0) ? cdt : ts0;
+		updateTimestamp(entryDest, i, version, greater1 + opCost);
+	}
+
+	return NULL;
+}
+
+void* logAssignment(UInt src, UInt dest) {
+	int level = getRegionLevel();
+	UInt version = getVersion();
+	int i = 0;
+	TEntry* entry0 = getLTEntry(src);
+	TEntry* entryDest = getLTEntry(dest);
+	
+	for (i = 0; i < level; i++) {
+		UInt64 cdt = getCdt(i);
+		UInt64 ts0 = getTimestamp(entry0, i, version);
+		UInt64 greater1 = (cdt > ts0) ? cdt : ts0;
+		updateTimestamp(entryDest, i, version, greater1);
+	}
+
+	return NULL;
+
+}
+
+void* logAssignmentConst(UInt dest) {
+	int level = getRegionLevel();
+	UInt version = getVersion();
+	int i = 0;
+	TEntry* entryDest = getLTEntry(dest);
+	
+	for (i = 0; i < level; i++) {
+		UInt64 cdt = getCdt(i);
+		updateTimestamp(entryDest, i, version, cdt);
+	}
+
+	return NULL;
+
+}
+
+void* logInsertValue(UInt src_addr, UInt dst_addr);
+void* logInsertValueConst(UInt dst_addr);
+
+void* logLoadInst(const void* src_addr, UInt dest);
+void* logStoreInst(UInt src, const void* dest_addr);
+
+void logPhiNode(UInt dest, UInt num_incoming_values, UInt num_t_inits, ...);
+
+void addControlDep(UInt cond);
+void removeControlDep();
+
+void addReturnValueLink(UInt dest);
+void logFuncReturn(UInt src);
+
+void linkArgToLocal(UInt src);
+void linkArgToConst();
+void transferAndUnlinkArg(UInt dest);
+
+UInt logLibraryCall(UInt cost, UInt dest, UInt num_in, ...);
+
+void logBBVisit(UInt bb_id);
+
+
+void initProfiler();
+void deinitProfiler();
+
+void logRegionEntry(UInt region_id, UInt region_type);
+void logRegionExit(UInt region_id, UInt region_type);
+
+
+
+
+
+
+/* The following functions are deprecated. Don't bother implementing them */
+void linkInitToCondition(const void* rhs, const void* lhs);
+UInt logInductionVarDependence(const void* induct_var);
+
+UInt logOutputToConsole(UInt id, UInt bb_id, int num_out, ...);
+UInt logInputFromConsole(UInt id, UInt bb_id, int num_in, ...);
+
+void stackVariableAlloc(UInt bb_id, const void* address);
+void stackVariableDealloc(UInt bb_id, const void* address);
+
+void linkAddrToArgName(UInt bb_id, const void* address, char* argname);
+
+void setReturnTimestampConst();
+void setReturnTimestamp(UInt bb_id, const void* retval);
+void getReturnTimestamp(UInt id, UInt bb_id, const void* address);
+
+void onBasicBlockEntry(UInt bb_id);
+
+void openOutputFile();
+void closeOutputFile();
+
+void linkInit(UInt cond);
+void removeInit();
+
+void printProfileData(void);
+
