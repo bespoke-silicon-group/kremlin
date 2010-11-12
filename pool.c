d4c702e51c7ffd946ab2bc1e64a8c0b9ed5c5cdf
Chris Louie <cmlouie@ucsd.edu>
2010-11-12 23:33:06 +0000

    The pool dynamically grows. PoolReserve(size_t size) will allocate at least the amount specified in preperation. 4 GB malloc limit handled properly

--- pool.c
+++ pool.c
@@ -7,111 +7,191 @@
 #define TRUE 1
 #define FALSE 0
 
-#define MAX_ALLOC_SIZE	(1024*1024*1024)
+#define MAX_ALLOC_SIZE  (1024*1024*1024)
+#define DEFAULT_PAGE_COUNT  16
+
+typedef struct SubPool SubPool;
 
 struct Pool
 {
-	size_t pageSize;
-	size_t pageCount;
-	Pool* next;
-	vector* freeList;
-	void * highAddr;
-	unsigned char data[];
+    /// The size of the memory returned from this pool.
+    size_t pageSize;
+
+    /// The number of pages in the pool.
+    size_t pageCount;
+
+    /// The list of free pages.
+    vector* freeList;
+
+    /// Sub-pools of memory this pool can alloc/free memory from.
+    vector* subPools;
 };
 
+struct SubPool
+{
+    /// The pool that owns this sub pool.
+    Pool* parent;
 
+    /// The size of allocated data.
+    size_t size;
+
+    /// The space to allocate from.
+    unsigned char data[];
+};
+
+int SubPoolCreate(SubPool** p, Pool* parent, size_t size);
+int SubPoolDelete(SubPool** p);
+
+int PoolFreeListPush(Pool* p, void* ptr);
+void* PoolFreeListPop(Pool* p);
+
+/**
+ * Creates a new SubPool. This pool contains pages of size pageSize according
+ * the pool this is within.
+ *
+ * @param p *p will point to the newly allocated pool.
+ * @param parent The pool this sub pool belongs to.
+ *
+ * @return TRUE on success.
+ */
+int SubPoolCreate(SubPool** p, Pool* parent, size_t size)
+{
+    unsigned char* page;
+
+    if(!(*p = (SubPool*)malloc(sizeof(SubPool) + size)))
+        return FALSE;
+
+    (*p)->parent = parent;
+    (*p)->size = size;
+
+    return TRUE;
+}
+
+/**
+ * Deletes a subpool.
+ *
+ * This will deallocate any properly constructed structure in the sub pool. If
+ * a structure is not constructed, it will be safely ignored.
+ */
+int SubPoolDelete(SubPool** p)
+{
+    if(!p || !*p)
+        return TRUE;
+
+    free(*p);
+    *p = NULL;
+
+    return TRUE;
+}
 
 int PoolCreate(Pool** p, size_t pageCount, size_t pageSize)
 {
-	unsigned char* page;
+    unsigned char* page;
 
-	assert(sizeof(size_t) >= sizeof(unsigned long long));
-	size_t size = sizeof(Pool) + pageSize * pageCount;
-	if(!((*p) = (Pool*)malloc(sizeof(Pool))))
-	{
-		assert(0 && "Failed to alloc pool.");
-		return FALSE;
-	}
-	size_t highAddr = ((size_t)*p) + size;
-	fprintf(stderr, "PoolCreate: addr = 0x%llx to 0x%llx, size = %lld page = %lld, count = %lld\n", *p, highAddr, size, pageSize, pageCount);
-	//bzero(*p, size);
+    assert(sizeof(size_t) >= sizeof(unsigned long long) && "Requires 64-bit size_t");
 
-	(*p)->next = NULL;
-	(*p)->pageSize = pageSize;
-	(*p)->pageCount = pageCount;
-	(*p)->highAddr = (void *)highAddr;
-	if(!vector_create(&(*p)->freeList, NULL, NULL))
-	{
-		assert(0 && "Failed to alloc free list.");
-		return FALSE;
-	}
+    if(!((*p) = (Pool*)malloc(sizeof(Pool))))
+    {
+        assert(0 && "Failed to alloc pool.");
+        return FALSE;
+    }
 
-	if(!vector_reserve((*p)->freeList, pageCount))
-	{
-		assert(0 && "Failed to reserve space in free list.");
-		return FALSE;
-	}
+    (*p)->pageSize = pageSize;
+    (*p)->pageCount = DEFAULT_PAGE_COUNT; // the pageCount argument is ignored because this pool grows dynamically.
 
-	unsigned long long pagesAllocated = 0;
- 	size_t memoryToAllocate = pageSize * pageCount;
-	while (memoryToAllocate > 0) {
-		size_t currentAllocate = (memoryToAllocate < MAX_ALLOC_SIZE) ? memoryToAllocate : MAX_ALLOC_SIZE;
-		char *allocated = malloc(currentAllocate);
-		char *current;
+    if(!vector_create(&(*p)->freeList, NULL, NULL))
+    {
+        assert(0 && "Failed to alloc subPool list.");
+        return FALSE;
+    }
+    
+    if(!vector_create(&(*p)->subPools, NULL, NULL))
+    {
+        assert(0 && "Failed to alloc subPool list.");
+        return FALSE;
+    }
 
-		fprintf(stderr, "allocated = 0x%llx size = %lld\n", allocated, currentAllocate);
-		for (current = allocated; current < allocated + currentAllocate; current +=pageSize) {
-			vector_push((*p)->freeList, current);
-			pagesAllocated++;
-		}
-		memoryToAllocate -= currentAllocate;
-	}
-	fprintf(stderr, "bzero done..\n");
-	/*
-	for(page = (*p)->data; page < (unsigned char*)(*p)->data + (pageCount-1) * pageSize; page += pageSize)
-	{
-		vector_push((*p)->freeList, page);
-		pagesAllocated++;
-	}*/
-
-	fprintf(stderr, "PoolCreate - pagesAllocated: %llu\n", pagesAllocated);
-
-	return TRUE;
+    return TRUE;
 }
 
+/**
+ * Allocates a new large chunk of memory and adds it to the pool of memory we
+ * can allocate from.
+ */
+int PoolAllocNewSubPool(Pool* p)
+{
+    unsigned char* page;
+    SubPool* newPool;
+    size_t pageCount = p->pageCount;
+    size_t pageSize = p->pageSize;
+
+    size_t size = pageSize * pageCount;
+    if(size > MAX_ALLOC_SIZE)
+        size = MAX_ALLOC_SIZE;
+
+    p->pageCount += size / pageSize;
+
+    SubPoolCreate(&newPool, p, size);
+    vector_push(p->subPools, newPool);
+
+    // Add the pages from the newly allocated pool to our free list.
+    for(page = newPool->data; page < (unsigned char*)newPool->data + (pageCount-1) * pageSize; page += pageSize)
+    {
+        if(!(PoolFreeListPush(p, page)))
+        {
+            fprintf(stderr, "Failed to add page to pool's free list!\n");
+            assert(0);
+        }
+    }
+
+    return TRUE;
+}
+
+// XXX: IMPLEMENT PROPERLY
 int PoolDelete(Pool** p)
 {
-	if(!p || !*p)
-		return FALSE;
+    if(!p || !*p)
+        return FALSE;
 
-	if(vector_size((*p)->freeList) != (*p)->pageCount)
-	{
-		fprintf(stderr, "WARNING: Deleting pool before freeing all memory allocated from the pool.\n");
-		fprintf(stderr, "%llu references remain\n.", (unsigned long long)(*p)->pageCount - vector_size((*p)->freeList));
-	}
-	if(!vector_delete(&(*p)->freeList))
-		assert(0 && "vector_delete failed!\n");
+    free(*p);
+    *p = NULL;
 
-	free(*p);
-	*p = NULL;
-
-	return TRUE;
+    return TRUE;
 }
 
 void* PoolMalloc(Pool* p)
 {
-	void* ret = vector_pop(p->freeList);
-	assert(ret && "Pool ran out of memory");
-//	fprintf(stderr, "PoolMalloc addr = 0x%llx\n", ret);
-//	assert(ret >= p->data && ret <= p->highAddr);
-	
-	return ret;
+    void* ret;
+
+    // Try to get something from the free list.
+    if(ret = PoolFreeListPop(p))
+        return ret;
+
+    // If the free list had nothing, allocate a new pool and try again.
+    PoolAllocNewSubPool(p);
+    return PoolMalloc(p);
 }
 
 void PoolFree(Pool* p, void* ptr)
 {
+    //assert(ptr >= *p->data && (unsigned char*)ptr < (unsigned char*)p->data + p->pageCount * p->pageSize);
 
-	//assert(ptr >= *p->data && (unsigned char*)ptr < (unsigned char*)p->data + p->pageCount * p->pageSize);
-
-	vector_push(p->freeList, ptr);
+    PoolFreeListPush(p, ptr);
 }
+
+int PoolFreeListPush(Pool* p, void* ptr)
+{
+    return vector_push(p->freeList, ptr);
+}
+
+void* PoolFreeListPop(Pool* p)
+{
+    return vector_pop(p->freeList);
+}
+
+void PoolReserve(Pool* p, size_t pageCount)
+{
+    while(p->pageCount < pageCount)
+        PoolAllocNewSubPool(p);
+}
+
--- pool.h
+++ pool.h
@@ -5,10 +5,43 @@
 
 typedef struct Pool Pool;
 
+/**
+ * Creates a new fixed-size page pool.
+ * @param p 		Where to allocate the pool. *p will point to the newly 
+ * 					created pool.
+ * @param pageCount A suggesion on how many pages are expected.
+ * @param pageSize	The fixed size of the pages in this pool.
+ * @return TRUE on success.
+ */
 int PoolCreate(Pool** p, size_t pageCount, size_t pageSize);
+
+/**
+ * Deallocates the pool. All memory allocated in the pool will be invalid
+ * after this call.
+ *
+ * @param p			Address of the pool pointer. It will be set to NULL.
+ * @return TRUE on success.
+ */
 int PoolDelete(Pool** p);
 
+/**
+ * Allocates a page from the pool.
+ * @param p 		The pool.
+ * @return			A pointer to the allocated page in the pool.
+ */
 void* PoolMalloc(Pool* p);
+
+/**
+ * Frees a page in the pool.
+ * @param p			The pool.
+ * @param ptr		The pointer returned from PoolMalloc.
 void PoolFree(Pool* p, void* ptr);
+
+/**
+ * Reserves at least pageCount pages for the pool.
+ * @param p			The pool.
+ * @param pageCount	The amount of pages to allocate now.
+ */
+void PoolReserve(Pool* p, size_t pageCount);
 
 #endif /* POOL_H */
