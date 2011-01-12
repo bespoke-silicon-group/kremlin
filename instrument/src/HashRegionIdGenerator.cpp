54a84bd32a00610e1d71eaa4a83b495e0456c0a4
Sat Garcia <sat@cs.ucsd.edu>
2011-01-12 19:47:32 +0000

    masks away the highest bit of region id since planner only supports signed 64 bit

--- instrument/src/HashRegionIdGenerator.cpp
+++ instrument/src/HashRegionIdGenerator.cpp
@@ -24,6 +24,11 @@ RegionId HashRegionIdGenerator::operator()(const std::string& name)
 	for(size_t i = 0; i < sizeof(boost::uuids::uuid) / sizeof(RegionId); i++)
 		region_id ^= ((RegionId*)id.data)[i];
 
+	// XXX FIXME: this is an ugly hack needed since Java doesn't support unsigned 64 bit nums.
+	// This should go away when DJ implements support in the planner (which uses Java) for
+	// unsigned 64 bit.
+	region_id = region_id & 0x7FFFFFFFFFFFFFFF;
+
 	return region_id;
 }
 
