7bc87e41ea9d0b0afb32391538e60d3ec9d3f013
Chris Louie <cmlouie@ucsd.edu>
2011-01-19 03:11:56 +0000

    The mem map allocator now grows dynamically as it uses more memory. It grows linearly.

--- runtime/src/MemMapAllocator.c
+++ runtime/src/MemMapAllocator.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/mman.h>
+#include "vector.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -31,7 +32,7 @@
 struct MemMapAllocator
 {
     /// The memory the pool can allocate from.
-    unsigned char* data;
+    vector* data;
 
     /// Points to the next memory location to allocate.
     unsigned char* freeListHead;
@@ -40,60 +41,95 @@ struct MemMapAllocator
     size_t size;
 };
 
+int MemMapAllocatorAllocAdditional(MemMapAllocator* p);
+
 /* -------------------------------------------------------------------------- 
  * Functions (alpha order).
  * ------------------------------------------------------------------------*/
-int MemMapAllocatorCreate(MemMapAllocator** p, size_t size)
+int MemMapAllocatorAllocAdditional(MemMapAllocator* p)
 {
     int protection = PROT_READ | PROT_WRITE;
     int flags = MAP_PRIVATE | MEM_MAP_POOL_ANON;
     int fileId = -1;
     int offset = 0;
 
+    // Allocate mmapped data.
+	unsigned char* data;
+    if((data = (unsigned char*)mmap(NULL, p->size, protection, flags, fileId, offset)) == MAP_FAILED)
+    {
+        char* errorString;
+        asprintf(&errorString, "MemMapAllocatorCreate - unable to mmap %llu bytes", (unsigned long long)p->size);
+        perror(errorString);
+        free(errorString);
+        errorString = NULL;
+        assert(0);
+        return FALSE;
+    }
+
+	if(!vector_push(p->data, data))
+	{
+		assert(0 && "Failed to push initial mmap data");
+		return FALSE;
+	}
+
+    p->freeListHead = vector_top(p->data);
+
+	return TRUE;
+}
+
+int MemMapAllocatorCreate(MemMapAllocator** p, size_t size)
+{
     if(!((*p = (MemMapAllocator*)malloc(sizeof(MemMapAllocator)))))
     {
         assert(0 && "MemMapAllocatorCreate - malloc failed.");
         return FALSE;
     }
 
-    // Allocate mmapped data.
-    if(((*p)->data = (unsigned char*)mmap(NULL, size, protection, flags, fileId, offset)) == MAP_FAILED)
-    {
-        char* errorString;
-        asprintf(&errorString, "MemMapAllocatorCreate - unable to mmap %llu bytes", (unsigned long long)size);
-        perror(errorString);
-        free(errorString);
-        errorString = NULL;
-        assert(0);
-        free(*p);
-        *p = NULL;
-        return FALSE;
-    }
+	if(!vector_create(&(*p)->data, NULL, NULL))
+	{
+		assert(0 && "MemMapAllocatorCreate - Failed to allocate vector for memmapped data");
+		MemMapAllocatorDelete(p);
+		return FALSE;
+	}
 
     // Initialize fields.
     (*p)->size = size;
-    (*p)->freeListHead = (*p)->data;
+
+	if(!MemMapAllocatorAllocAdditional(*p))
+		return FALSE;
 
     return TRUE;
 }
 
 int MemMapAllocatorDelete(MemMapAllocator** p)
 {
-	munmap((*p)->data, (*p)->size);
+	void** data;
+	void** data_end;
+	for(data = vector_begin((*p)->data), data_end = vector_end((*p)->data); data < data_end; data++)
+		munmap(*data, (*p)->size);
+
+	vector_delete(&(*p)->data);
+
 	free(*p);
 	p = NULL;
+
+	return TRUE;
 }
 
 void* MemMapAllocatorMalloc(MemMapAllocator* p, size_t size)
 {
     void* ret = p->freeListHead;
-    unsigned char* lastPtr = p->freeListHead;
     p->freeListHead += size;
-    assert(lastPtr < p->freeListHead && "Overflow detected!");
-    assert(p->freeListHead < p->data + p->size && "MemMapAllocator ran out of memory.");
+    if(p->freeListHead >= (unsigned char*)vector_top(p->data) + p->size)
+	{
+		if(!MemMapAllocatorAllocAdditional(p))
+			return FALSE;
+		return MemMapAllocatorMalloc(p, size);
+	}
     return ret;
 }
 
 void MemMapAllocatorFree(MemMapAllocator* p, void* ptr)
 {
+	// TODO: implement
 }
--- runtime/src/MemMapAllocator.h
+++ runtime/src/MemMapAllocator.h
@@ -5,9 +5,31 @@
 
 typedef struct MemMapAllocator MemMapAllocator;
 
+/**
+ * Creates a memory allocator backed by memmap.
+ *
+ * @param p 		*p will point to the newly allocated allocator or NULL if 
+ * 					creation fails.
+ * @param size 		The chunk size to grow the pool by.
+ * @return TRUE on success.
+ */
 int MemMapAllocatorCreate(MemMapAllocator** p, size_t size);
+
+/**
+ * Deletes the allocator.
+ *
+ * @return TRUE on success.
+ */
 int MemMapAllocatorDelete(MemMapAllocator** p);
+
+/**
+ * Allocate some memory from the allocator.
+ */
 void* MemMapAllocatorMalloc(MemMapAllocator* p, size_t size);
+
+/**
+ * Free memory allocated from the allocator.
+ */
 void MemMapAllocatorFree(MemMapAllocator* p, void* ptr);
 
 #endif /* MEM_MAP_ALLOCATOR_H */
