d51940c3dfe73036e711f5ab4bef7b577e11fad8
Sat Garcia <sat@cs.ucsd.edu>
2011-02-08 02:56:35 +0000

    spaces instead of tabs so gfortran doesn't barf all over the output with warnings

--- test/fortran/func_and_subroutine/main.f95
+++ test/fortran/func_and_subroutine/main.f95
@@ -4,13 +4,13 @@ implicit none
 integer :: x
 ! output
 integer :: doubleme
-	doubleme = x * 2
+      doubleme = x * 2
 end
 
 subroutine subby(x,y,z)
 implicit none
 integer :: x,y,z
-	z = x - y
+      z = x - y
 end
 
 program main
@@ -18,10 +18,10 @@ implicit none
 integer :: x, y, z
 integer doubleme
 
-	print*, "Enter a number for x: "
-	read*, x
-	y = doubleme(x)
-	print*, "Enter a number for z: "
-	call subby(x,y,z)
-	print*, "x = ", x, ", y = ", y, ", z = ", z
+      print*, "Enter a number for x: "
+      read*, x
+      y = doubleme(x)
+      print*, "Enter a number for z: "
+      call subby(x,y,z)
+      print*, "x = ", x, ", y = ", y, ", z = ", z
 end program main
