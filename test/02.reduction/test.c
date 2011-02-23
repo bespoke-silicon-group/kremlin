770d7f4c5ce6786404ae03cb5d2992a0be22f237
Donghwan Jeon <djeon@cs.ucsd.edu>
2011-02-23 20:46:27 +0000

    test 3: coarse grained doall

--- test/02.reduction/test.c
+++ test/02.reduction/test.c
@@ -4,23 +4,24 @@
 int* pointer1;
 
 
-int loopTest(int x) {
+int* loopTest(int x) {
 	int i, j;
-	int sum = 0;
 	pointer1 = malloc(x * sizeof(int));
 	for (i=0; i<x; i++) {
-		sum += pointer1[i];
+		double temp1, temp2, temp3;
+		temp3 = x + 0.1;
+		temp1 = (i+1.0) * (1.34 + i) / (i- 0.01);
+		temp2 = (i+1.11) * (1.35 + i) / (i- 0.02);
+		temp3 = (temp2 + temp1) * x * temp1 * temp2;
+		pointer1[i] = pointer1[i] + sin(temp3);
 	}
-
-		
-	
-	return sum;
+	return pointer1;
 	
 }
 
 
 int main() {
-	int res = loopTest(100); // sp should be close to 100
-	printf("res = %d\n", res);
+	int* res = loopTest(100); // sp should be close to 100
+	printf("res = %.2f\n", res[0]);
 	return 0;
 }
