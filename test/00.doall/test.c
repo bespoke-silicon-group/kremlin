aaeed9aab71e719b2432221cb888c85d5f559812
Donghwan Jeon <djeon@cs.ucsd.edu>
2011-08-05 08:44:05 +0000

    RShadowCopy bug fixed

--- test/00.doall/test.c
+++ test/00.doall/test.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* loopTest(int x) __attribute__ ((noinline));
+int* loopTest(int x, int count) __attribute__ ((noinline));
+int getSum(int x, int count) __attribute__ ((noinline));
 
 int* pointer;
 
 
-int* loopTest(int x) {
+int* loopTest(int x, int count) {
 	int i;
-	pointer = malloc(x * sizeof(int));
+	pointer = malloc(100 * sizeof(int));
 	for (i=0; i<x; i++) {
-		pointer[i] = pointer[i] + i;
+		pointer[i] = i + count;
 	}
 	return pointer;
 	
 }
 
+int getSum(int x, int y) {
+	return x + y;
+}
 
-int main() {
-	loopTest(100); // sp should be close to 100
-	loopTest(1000); // sp should be close to 1000
+
+int main(int argc, char** argv) {
+	int x = argc + 3;
+	printf("argc = %d\n", argc);
+	int ret = getSum(x, 3);
+	ret += getSum(x, 5);
+	ret += getSum(x, 8);
+	int* ret2 = loopTest(10, x); // sp should be close to 100
+	//loopTest(1000); // sp should be close to 1000
+	printf("addr = 0x%x 0x%x\n", ret, ret2);
 	return 0;
 }
