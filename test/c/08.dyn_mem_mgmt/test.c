7a1ed164b8a7af07734a0360d5f2e5472ee81e19
Sat Garcia <sat@cs.ucsd.edu>
2012-03-14 17:27:33 -0700

    renamed malloc test and included realloc for a more complete "basic dynamic memory management" test

--- test/c/08.dyn_mem_mgmt/test.c
+++ test/c/08.dyn_mem_mgmt/test.c
@@ -1,7 +1,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-
 #define NUM_POINTERS	100
 
 int* pointers[NUM_POINTERS];
@@ -9,9 +8,12 @@ int* pointers[NUM_POINTERS];
 int main() {
 	int i;
 	for (i=0; i<NUM_POINTERS; i++) {
-		pointers[i] = malloc(20);
+		pointers[i] = malloc(10*sizeof(int));
 	}
 
+	for (i=0; i<NUM_POINTERS; i++) {
+		pointers[i] = realloc(pointers[i],20*sizeof(int));
+	}
 	
 	for (i=0; i<NUM_POINTERS; i++) {
 		free(pointers[i]);
