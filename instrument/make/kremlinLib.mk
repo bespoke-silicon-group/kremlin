93aee1be6920cfb13a7582736735536ecd2ded4c
Sat Garcia <sat@cs.ucsd.edu>
2012-03-16 16:28:53 -0700

    got rid of crusty-old depth-finder build rules

--- instrument/make/kremlinLib.mk
+++ instrument/make/kremlinLib.mk
@@ -5,8 +5,6 @@
 # Kremlin library
 KREMLIN_LIB = kremlin.a
 
-DF_LIB = depth-finder.a
-
 # ---------------------------------------------------------------------------
 # Includes
 # ---------------------------------------------------------------------------
@@ -24,21 +22,11 @@ COMMON_SOURCES_WITH_PATH = $(addprefix $(KREMLIN_COMMON_SRC)/, $(COMMON_SOURCES)
 KREMLIN_SOURCES = debug.c kremlin.c ATable.c MemMapAllocator.c CRegion.c arg.c MShadowBase.c MShadowSkadu.c MShadowSTV.c compression.c config.c minilzo.c mpool.c MShadowStat.c MShadowDummy.c
 KREMLIN_SOURCES_WITH_PATH = $(addprefix $(KREMLIN_RUNTIME_SRC_DIR)/, $(KREMLIN_SOURCES))
 
-# All the max depth finder files
-#DF_SOURCES = depth-finder.c
-DF_SOURCES = depth-finder.c
-DF_SOURCES_WITH_PATH = $(addprefix $(KREMLIN_RUNTIME_SRC_DIR)/, $(DF_SOURCES))
-
 # All the sources required to build the kremlin library.
 ALL_KREMLIN_SOURCES_WITH_PATH = $(COMMON_SOURCES_WITH_PATH) $(KREMLIN_SOURCES_WITH_PATH)
 ALL_KREMLIN_OBJECTS_WITH_PATH = $(ALL_KREMLIN_SOURCES_WITH_PATH:.c=.o)
 
-ALL_DF_SOURCES_WITH_PATH = $(COMMON_SOURCES_WITH_PATH) $(DF_SOURCES_WITH_PATH)
-ALL_DF_OBJECTS_WITH_PATH = $(ALL_DF_SOURCES_WITH_PATH:.c=.o)
-
 ALL_KREMLIN_OBJECTS_IN_LIB = $(patsubst %, $(KREMLIN_LIB)(%), $(ALL_KREMLIN_OBJECTS_WITH_PATH))
-
-ALL_DF_OBJECTS_IN_LIB = $(patsubst %, $(DF_LIB)(%), $(ALL_DF_OBJECTS_WITH_PATH))
 
 # ---------------------------------------------------------------------------
 # Rules (alpha order)
@@ -51,17 +39,9 @@ $(ALL_KREMLIN_OBJECTS_WITH_PATH): CC = cc
 $(ALL_KREMLIN_OBJECTS_WITH_PATH): %.o: %.c
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-$(ALL_DF_OBJECTS_WITH_PATH): CFLAGS += -I$(KREMLIN_COMMON_SRC)
-$(ALL_DF_OBJECTS_WITH_PATH): CC = cc
-$(ALL_DF_OBJECTS_WITH_PATH): %.o: %.c
-	$(CC) $(CFLAGS) -c -o $@ $<
-
 # Archives the kremlin objects into a static library
 $(KREMLIN_LIB): $(ALL_KREMLIN_OBJECTS_IN_LIB)
 	ranlib $@
 
-$(DF_LIB): $(ALL_DF_OBJECTS_IN_LIB)
-	ranlib $@
-
 clean::
-	-@$(RM) $(KREMLIN_LIB) $(DF_LIB)
+	-@$(RM) $(KREMLIN_LIB)
