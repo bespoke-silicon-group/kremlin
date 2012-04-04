baffb0c7163db9aa51e3723f775e8bf670d5a46a
Sat Garcia <sat@cs.ucsd.edu>
2012-04-04 15:22:04 -0700

    readability refactoring

--- instrument/src/Placer.cpp
+++ instrument/src/Placer.cpp
@@ -54,10 +54,10 @@ void Placer::placeInsts()
     // placed yet.
     //
     // We initialize all the nodes' unplaced_users and identify the set of
-    // nodes that have no users and add them to the placeable set.
+    // nodes that have no users and add them to the placeable_nodes set.
     //
-    // We then continue to loop through the placeable set while it has
-    // elements. We pop off some node in the placeable list and iterate
+    // We then continue to loop through the placeable_nodes set while it has
+    // elements. We pop off some node in the placeable_nodes list and iterate
     // through its uses and decrement their unplaced_users since this is about
     // to be placed. If any of these unplaced_users drop to zero, we add them
     // to the placable set.
@@ -74,65 +74,72 @@ void Placer::placeInsts()
     LOG_INFO() << "Placing begins\n";
 
     // Initialize the nodes and create an initial placable set.
-    vector<Node*> placeable;
-    foreach(Nodes::value_type p, nodes)
+    vector<Node*> placeable_nodes;
+    foreach(Nodes::value_type inst_node_pair, nodes)
     {
-        Node& node = *p->second;
-        node.unplaced_users = node.users.size();
-        if(!node.unplaced_users)
-            placeable.push_back(&node);
+        Node& node_to_init = *inst_node_pair->second;
+        node_to_init.unplaced_users = node_to_init.users.size();
+        if(node_to_init.unplaced_users == 0)
+            placeable_nodes.push_back(&node_to_init);
 
-        DEBUG(LOG_DEBUG() << "To place: " << node.inst << "\n");
+        DEBUG(LOG_DEBUG() << "To place: " << node_to_init.inst << "\n");
 
-        foreach(Node* user, node.users)
+        foreach(Node* user, node_to_init.users)
             DEBUG(LOG_DEBUG() << "user: " << user->inst << "\n");
 
-        foreach(Node* dep, node.dependencies)
+        foreach(Node* dep, node_to_init.dependencies)
             DEBUG(LOG_DEBUG() << "dep: " << dep->inst << "\n");
     }
 
     // Loop until everything is placed.
-    while(placeable.size())
+    while(!placeable_nodes.empty())
     {
-        Node& node = *placeable.back();
-        placeable.pop_back();
+        Node& node_to_be_placed = *placeable_nodes.back();
+        placeable_nodes.pop_back();
 
         // Decrement the unplaced_users of the dependencies.
-        foreach(Node* dep, node.dependencies)
-            if(!--(dep->unplaced_users))
-                placeable.push_back(dep);
+        foreach(Node* dep, node_to_be_placed.dependencies)
+		{
+			--dep->unplaced_users;
+            if(dep->unplaced_users == 0)
+                placeable_nodes.push_back(dep);
+		}
 
-        DEBUG(LOG_DEBUG() << "Checking if placed: " << node.inst << " parent: " << node.inst.getParent() << "\n");
+        DEBUG(LOG_DEBUG() << "Checking if placed: " << node_to_be_placed.inst
+		<< " parent: " << node_to_be_placed.inst.getParent() << "\n");
 
         // if not placed.
-        if(!node.inst.getParent())
+        if(node_to_be_placed.inst.getParent() == NULL)
         {
-            std::set<Instruction*> users;
-            foreach(Node* user, node.users)
-                users.insert(&user->inst);
+            std::set<Instruction*> user_insts;
+            foreach(Node* user, node_to_be_placed.users)
+                user_insts.insert(&user->inst);
 
             LOG_DEBUG() << "Getting common dom\n";
 
             // Identify the nearest common dominator.
-            assert(users.begin() != users.end());
-            BasicBlock* common_dom = (*users.begin())->getParent();
-            foreach(Instruction* user, users)
+            assert(user_insts.begin() != user_insts.end());
+            BasicBlock* common_dom = (*user_insts.begin())->getParent();
+            foreach(Instruction* user, user_insts)
                 common_dom = dt.findNearestCommonDominator(common_dom, user->getParent());
 
             LOG_DEBUG() << "Getting earliest inst\n";
 
             // Identify the earliest user in the BB or the terminator.
-            Instruction* earliest_user = common_dom->getTerminator();
+            Instruction* earliest_user_inst = common_dom->getTerminator();
             foreach(Instruction& inst, *common_dom)
-                if(users.find(&inst) != users.end())
+			{
+                if(user_insts.find(&inst) != user_insts.end())
                 {
-                    earliest_user = &inst;
+                    earliest_user_inst = &inst;
                     break;
                 }
+			}
 
-            DEBUG(LOG_DEBUG() << "Placing " << node.inst << " before " << *earliest_user << "\n");
+            DEBUG(LOG_DEBUG() << "Placing " << node_to_be_placed.inst << 
+			" before " << *earliest_user_inst << "\n");
 
-            node.inst.insertBefore(earliest_user);
+            node_to_be_placed.inst.insertBefore(earliest_user_inst);
         }
     }
     LOG_INFO() << "Placing finishes\n";
@@ -148,11 +155,11 @@ void Placer::placeInsts()
 void Placer::addInstForPlacement(llvm::Instruction& call, const std::set<llvm::Instruction*>& users)
 {
     // Make the node.
-    Node& node = getOrCreateNode(call);
+    Node& inst_node = getOrCreateNode(call);
 
     // Add the users.
     foreach(Instruction* user, users)
-        node.addUser(getOrCreateNode(*user));
+        inst_node.addUser(getOrCreateNode(*user));
 }
 
 Placer::Node& Placer::getOrCreateNode(llvm::Instruction& inst)
