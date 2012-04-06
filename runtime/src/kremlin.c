c9d0d258a3774673b333d24894378deb60761013
Sat Garcia <sat@cs.ucsd.edu>
2012-04-06 15:18:52 -0700

    added "completed" debug printout to KStore (easier on the eyes that way)

--- runtime/src/kremlin.c
+++ runtime/src/kremlin.c
@@ -1370,7 +1370,7 @@ void* _KLoad0(Addr addr, Reg dest, UInt32 size) {
         RegionUpdateCp(region, value);
     }
 
-    MSG(3, "load ts[%u] over\n\n");
+    MSG(3, "load ts[%u] completed\n\n");
     return NULL;
 }
 
@@ -1445,6 +1445,7 @@ void* _KStore(UInt src, Addr dest_addr, UInt32 size) {
 
 	Level minLevel = getLevel(0);
 	MShadowSet(dest_addr, getIndexDepth(), RegionGetVArray(minLevel), tArray, size);
+    MSG(0, "store ts[0x%x] completed\n", dest_addr);
     return NULL;
 }
 
