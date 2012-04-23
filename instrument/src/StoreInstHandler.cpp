6f8752ea17666bd4eb2a22f338da101f316a728f
Sat Garcia <sat@cs.ucsd.edu>
2012-04-23 16:27:23 -0700

    Stores of constants now correctly use KStoreConst. Previously they used KStore and had a screwy source reg id.

--- instrument/src/StoreInstHandler.cpp
+++ instrument/src/StoreInstHandler.cpp
@@ -20,14 +20,20 @@ StoreInstHandler::StoreInstHandler(TimestampPlacer& ts_placer) :
     // Set up the opcodes
     opcodes += Instruction::Store;
 
-    // Setup the log_func
+    // Setup the storeRegFunc
     Module& m = *ts_placer.getFunc().getParent();
     LLVMTypes types(m.getContext());
     vector<const Type*> args;
     args += types.i32(), types.pi8(), types.i32();
-    FunctionType* func_type = FunctionType::get(types.voidTy(), args, false);
+    FunctionType* store_func_type = FunctionType::get(types.voidTy(), args, false);
 
-    log_func = cast<Function>(m.getOrInsertFunction("_KStore", func_type));
+    storeRegFunc = cast<Function>(m.getOrInsertFunction("_KStore", store_func_type));
+
+	args.clear();
+    args += types.pi8(), types.i32();
+    FunctionType* store_const_func_type = FunctionType::get(types.voidTy(), args, false);
+	
+    storeConstFunc = cast<Function>(m.getOrInsertFunction("_KStoreConst", store_const_func_type));
 }
 
 /**
@@ -55,7 +61,8 @@ void StoreInstHandler::handle(llvm::Instruction& inst)
 
     // first we get a ptr to the source
     Value& src = *si.getOperand(0);
-    args += ConstantInt::get(types.i32(),ts_placer.getId(src)); // src ID
+	if(!isa<Constant>(src))
+    	args += ConstantInt::get(types.i32(),ts_placer.getId(src)); // src ID
 
     // the dest is already in ptr form so we simply use that
     CastInst& cast_inst = *CastInst::CreatePointerCast(si.getPointerOperand(),types.pi8(),"inst_arg_ptr"); // dest addr
@@ -65,8 +72,15 @@ void StoreInstHandler::handle(llvm::Instruction& inst)
     args += ConstantInt::get(types.i32(),MemoryInstHelper::getTypeSizeInBytes(&si));
 
     // Add the cast, call and the timestamp to store.
-    CallInst& ci = *CallInst::Create(log_func, args.begin(), args.end(), "");
+	Function* proper_func = NULL;
+	if(isa<Constant>(src))
+		proper_func = storeConstFunc;
+	else
+		proper_func = storeRegFunc;
+
+    CallInst& ci = *CallInst::Create(proper_func, args.begin(), args.end(), "");
     ts_placer.constrainInstPlacement(cast_inst, ci);
     ts_placer.constrainInstPlacement(ci, inst);
-    ts_placer.requireValTimestampBeforeUser(src, ci);
+	if(!isa<Constant>(src))
+    	ts_placer.requireValTimestampBeforeUser(src, ci);
 }
