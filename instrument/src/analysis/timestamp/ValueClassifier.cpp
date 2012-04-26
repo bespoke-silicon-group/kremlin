cb3c31c55fe94356c5d33f684abf91aa12497031
Sat Garcia <sat@cs.ucsd.edu>
2012-04-26 12:57:37 -0700

    select inst is now handled like ALU ops: all inputs (condition, true value, false value) act as source dependences. This mimics what we did previously but is still slightly overconservative (probably not worth fixing though).

--- instrument/src/analysis/timestamp/ValueClassifier.cpp
+++ instrument/src/analysis/timestamp/ValueClassifier.cpp
@@ -28,6 +28,12 @@ ValueClassifier::Class ValueClassifier::operator()(llvm::Value* val) const
         isa<BitCastInst>(val) ||
         isa<CastInst>(val) ||
         isa<CmpInst>(val) ||
+		// @TRICKY: selects are like an if/else rolled into one instruction.
+		// This means they have some implicit phi which we don't handle. As a
+		// result of the implicit phi, we are a bit too conservative since
+		// only one of the operands (true or false) will be "taken" and
+		// therefore only one of them should be a data dependence.
+        isa<SelectInst>(val) ||
         isa<GetElementPtrInst>(val) ||
         isa<ReturnInst>(val) ||
         isa<StoreInst>(val))
