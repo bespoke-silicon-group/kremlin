49f9caac856c00fc5924a985dae789043eebfcd5
Sat Garcia <elsaturnino@gmail.com>
2013-06-17 12:42:36 -0700

    kremlin lib files are .cpp now (previously were .c)

--- instrument/make/kremlinLib.mk
+++ instrument/make/kremlinLib.mk
@@ -19,12 +19,12 @@ COMMON_SOURCES = #kremlin_deque.c #hash_map.c vector.c
 COMMON_SOURCES_WITH_PATH = $(addprefix $(KREMLIN_COMMON_SRC)/, $(COMMON_SOURCES))
 
 # All the kremlin runtime source files.
-KREMLIN_SOURCES = debug.c kremlin.c ATable.c MemMapAllocator.c CRegion.c arg.c MShadowBase.c MShadowSkadu.c MShadowSTV.c compression.c config.c minilzo.c mpool.c MShadowStat.c MShadowDummy.c MShadowCache.c
+KREMLIN_SOURCES = debug.cpp kremlin.cpp ATable.cpp MemMapAllocator.cpp CRegion.cpp arg.cpp MShadowBase.cpp MShadowSkadu.cpp MShadowSTV.cpp compression.cpp config.cpp minilzo.cpp mpool.cpp MShadowStat.cpp MShadowDummy.cpp MShadowCache.cpp
 KREMLIN_SOURCES_WITH_PATH = $(addprefix $(KREMLIN_RUNTIME_SRC_DIR)/, $(KREMLIN_SOURCES))
 
 # All the sources required to build the kremlin library.
 ALL_KREMLIN_SOURCES_WITH_PATH = $(COMMON_SOURCES_WITH_PATH) $(KREMLIN_SOURCES_WITH_PATH)
-ALL_KREMLIN_OBJECTS_WITH_PATH = $(ALL_KREMLIN_SOURCES_WITH_PATH:.c=.o)
+ALL_KREMLIN_OBJECTS_WITH_PATH = $(ALL_KREMLIN_SOURCES_WITH_PATH:.cpp=.o)
 
 ALL_KREMLIN_OBJECTS_IN_LIB = $(patsubst %, $(KREMLIN_LIB)(%), $(ALL_KREMLIN_OBJECTS_WITH_PATH))
 
@@ -36,7 +36,7 @@ ALL_KREMLIN_OBJECTS_IN_LIB = $(patsubst %, $(KREMLIN_LIB)(%), $(ALL_KREMLIN_OBJE
 #$(ALL_KREMLIN_OBJECTS_WITH_PATH): CFLAGS += -O3  -pg -I$(KREMLIN_COMMON_SRC) $(KREMLIB_EXTRA_FLAGS)
 $(ALL_KREMLIN_OBJECTS_WITH_PATH): CFLAGS += -O3  -Wall -I$(KREMLIN_COMMON_SRC) $(KREMLIB_EXTRA_FLAGS) -x c++
 $(ALL_KREMLIN_OBJECTS_WITH_PATH): CC = g++
-$(ALL_KREMLIN_OBJECTS_WITH_PATH): %.o: %.c
+$(ALL_KREMLIN_OBJECTS_WITH_PATH): %.o: %.cpp
 	$(CC) $(CFLAGS) -c -o $@ $<
 
 # Archives the kremlin objects into a static library
