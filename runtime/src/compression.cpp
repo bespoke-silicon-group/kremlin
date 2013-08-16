4bae24ab0bf0710f3b8904b8f32f7cc4b4593460
Sat Garcia <elsaturnino@gmail.com>
2013-08-15 22:34:45 -0700

    improving some doxygen comments and adding pre/post-conditions and invariants

--- runtime/src/compression.cpp
+++ runtime/src/compression.cpp
@@ -5,6 +5,7 @@
 #include "MShadowSkadu.h"
 #include "config.h"
 #include "minilzo.h"
+#include "debug.h"
 
 static UInt64 _compSrcSize;
 static UInt64 _compDestSize;
@@ -17,40 +18,44 @@ static UInt64 totalEvict;
 static HEAP_ALLOC(wrkmem, LZO1X_1_MEM_COMPRESS);
 
 
-UInt8* compressData(UInt8* src, lzo_uint src_size, lzo_uintp dest_size) {
-	assert(src != NULL);
-	assert(src_size > 0);
-	assert(dest_size != NULL);
+UInt8* compressData(UInt8* decomp_data, lzo_uint decomp_size, lzo_uintp comp_size) {
+	assert(decomp_data != NULL);
+	assert(decomp_size > 0);
+	assert(comp_size != NULL);
 
 	//XXX need a specialized memory allocator, for now, just check the 
 
-	//UInt8* dest = MemPoolAlloc();
-	UInt8* dest = (UInt8*)malloc(src_size);
-	int result = lzo1x_1_compress(src, src_size, dest, dest_size, wrkmem);
+	//UInt8 *comp_data = MemPoolAlloc();
+	UInt8 *comp_data = (UInt8*)malloc(decomp_size);
+	int result = lzo1x_1_compress(decomp_data, decomp_size, comp_data, comp_size, wrkmem);
 	assert(result == LZO_E_OK);
-	//memcpy(dest, src, src_size);
-	//*dest_size  = src_size;
+	//memcpy(comp_data, decomp_data, decomp_size);
+	//*comp_size  = decomp_size;
 
-	//fprintf(stderr, "compressed from %d to %d\n", src_size, *dest_size);
-	_compSrcSize += src_size;
-	_compDestSize += *dest_size;
-	return dest;
+	//MSG(3, "compressed from %d to %d\n", decomp_size, *comp_size);
+	_compSrcSize += decomp_size;
+	_compDestSize += *comp_size;
+
+	assert(comp_data != NULL);
+	return comp_data;
 }
 
-void decompressData(UInt8* dest, UInt8* src, lzo_uint src_size, lzo_uintp dest_size) {
-	assert(src != NULL);
-	assert(dest != NULL);
-	assert(src_size > 0);
-	assert(dest_size != NULL);
+void decompressData(UInt8* decomp_data, UInt8* comp_data, lzo_uint comp_size, lzo_uintp decomp_size) {
+	assert(comp_data != NULL);
+	assert(decomp_data != NULL);
+	assert(comp_size > 0);
+	assert(decomp_size != NULL);
 
-	int result = lzo1x_decompress(src, src_size, dest, dest_size, NULL);
+	int result = lzo1x_decompress(comp_data, comp_size, decomp_data, decomp_size, NULL);
 	assert(result == LZO_E_OK);
-	//memcpy(dest, src, src_size);
-	//*dest_size = src_size;
+	//memcpy(decomp_data, comp_data, comp_size);
+	//*decomp_size = comp_size;
 
-	//fprintf(stderr, "decompressed from %d to %d\n", src_size, *dest_size);
-	//MemPoolFree(src);
-	free(src);
+	//MSG(3, "decompressed from %d to %d\n", comp_size, *decomp_size);
+	//MemPoolFree(comp_data);
+	free(comp_data);
+	comp_data = NULL;
+	assert(comp_data == NULL);
 }
 
 /*! \brief Unknown.
@@ -66,12 +71,16 @@ static inline int getByteOffset(int a, int b) {
 
 
 // TODO: make this a member function of Level Table
-/*! \brief Get the number of entries in Level table.
+/*! @brief Get the number of entries in Level table.
  *
- * \param l_table The table to get number of entries from.
- * \return Number of entries in specified level table.
+ * @param l_table The table to get number of entries from.
+ * @return Number of entries in specified level table.
+ * @pre l_table is non-NULL.
+ * @pre At least one TimeTable* in l_table is NULL.
  */
 int getTimeTableSize(LevelTable* l_table) {
+	assert(l_table != NULL);
+	// TODO: assert for NULL TimeTable* precondition
 	for (unsigned i = 0; i < LevelTable::MAX_LEVEL; ++i) {
 		TimeTable* table = l_table->tArray[i];
 		if (table == NULL)
@@ -88,7 +97,16 @@ int getTimeTableSize(LevelTable* l_table) {
 class ActiveSetEntry {
 public:
 	UInt16 r_bit;
-	UInt32 code;
+	UInt32 code; // TODO: make this debug mode only
+	ActiveSetEntry() : r_bit(1), code(0xDEADBEEF) {}
+	~ActiveSetEntry() {}
+
+	void* operator new(size_t size) {
+		return MemPoolAllocSmall(sizeof(ActiveSetEntry));
+	}
+	void operator delete(void *ptr) {
+		MemPoolFreeSmall(ptr, sizeof(ActiveSetEntry));
+	}
 };
 
 static std::map<LevelTable*, ActiveSetEntry*> active_set;
@@ -106,36 +124,18 @@ static inline void printActiveSet() {
 	unsigned i = 0;
 	for(std::map<LevelTable*, ActiveSetEntry*>::iterator it = active_set.begin(); 
 			it != active_set.end(); ++it, ++i) {
-		if (it == active_set_clock_hand) fprintf(stderr,"*");
-		fprintf(stderr,"%u: key = %p, r_bit = %hu\n", i, it->first, it->second->r_bit);
+		if (it == active_set_clock_hand) MSG(3,"*");
+		MSG(3,"%u: key = %p, r_bit = %hu\n", i, it->first, it->second->r_bit);
 	}
 }
 
-/*! \brief Allocate memory for a new active set entry.
- *
- * \return The newly allocated active set entry.
- */
-static inline ActiveSetEntry* ActiveSetEntryAlloc() {
-	ActiveSetEntry *as = (ActiveSetEntry*)MemPoolAllocSmall(sizeof(ActiveSetEntry));
-	as->r_bit = 1;
-	as->code = 0xDEADBEEF;
-	return as;
-}
+void CBuffer::init(unsigned size) {
+	assert(size > 0);
+	if (KConfigGetCompression() == 0) return;
 
-/*! \brief Deallocates (i.e. frees) memory for active set entry
- *
- * \param entry The active set entry to deallocate.
- */
-static inline void ActiveSetEntryFree(ActiveSetEntry* entry) {
-	MemPoolFreeSmall(entry, sizeof(ActiveSetEntry));
-}
-
-void CBuffer::init(int size) {
-	if (KConfigGetCompression() == 0)
-		return;
-
-	fprintf(stderr,"Initializing compression buffer to size %d\n",size);
+	MSG(2,"Initializing compression buffer to size %d\n",size);
 	
+	// TODO: make this an exception
 	if (lzo_init() != LZO_E_OK) {
 		printf("internal error - lzo_init() failed !!!\n");
 	    printf("(this usually indicates a compiler bug - try recompiling\nwithout optimizations, and enable '-DLZO_DEBUG' for diagnostics)\n");
@@ -146,9 +146,9 @@ void CBuffer::init(int size) {
 }
 
 void CBuffer::deinit() {
-	fprintf(stderr, "CBuffer (evict / access / ratio) = %llu, %llu, %.2f\n",
+	MSG(2, "CBuffer (evict / access / ratio) = %llu, %llu, %.2f\n",
 		totalEvict, totalAccess, ((double)totalEvict / totalAccess) * 100.0);
-	fprintf(stderr, "Compression Overall Rate = %.2f X\n", (double)_compSrcSize / _compDestSize);
+	MSG(2, "Compression Overall Rate = %.2f X\n", (double)_compSrcSize / _compDestSize);
 }
 
 /*! \brief Find an entry to remove from active set.
@@ -161,54 +161,63 @@ std::map<LevelTable*, ActiveSetEntry*>::iterator getVictim() {
 	// set active_set_clock_hand to entry that will be removed
 	while(active_set_clock_hand->second->r_bit == 1) {
 		active_set_clock_hand->second->r_bit = 0;
+		// TODO: these asserts need to go bye-bye
 		assert(active_set_clock_hand->second->code == 0xDEADBEEF);
 		assert(active_set_clock_hand->first->code == 0xDEADBEEF);
 		advanceClockHand();
 	}
 
+	// TODO: these asserts need to go bye-bye
 	assert(active_set_clock_hand->first->code == 0xDEADBEEF);
 	assert(active_set_clock_hand->second->code == 0xDEADBEEF);
+
 	std::map<LevelTable*, ActiveSetEntry*>::iterator ret = active_set_clock_hand;
 	advanceClockHand();
 	return ret;
 }
 
-void CBuffer::addToBuffer(LevelTable* l_table) {
+void CBuffer::addToBuffer(LevelTable *l_table) {
+	assert(l_table != NULL);
+
 	//fprintf(stderr, "adding l_table 0x%llx\n", l_table);
-	ActiveSetEntry *as = ActiveSetEntryAlloc();
+	ActiveSetEntry *as = new ActiveSetEntry();
 	active_set.insert( std::pair<LevelTable*, ActiveSetEntry*>(l_table, as) );
 
 	// TRICKY: size will only be 1 the first time we add something to the
 	// buffer so we'll go ahead and initialize the clock hand to that first
 	// entry
 	if (active_set.size() == 1) active_set_clock_hand = active_set.begin();
+	// TODO: assert active_size size is less than num_entries
 }
 
 int CBuffer::evictFromBuffer() {
 	std::map<LevelTable*, ActiveSetEntry*>::iterator victim = getVictim();
 	assert(victim->second->code == 0xDEADBEEF);
 	LevelTable* lTable = victim->first;
-	assert(lTable->code == 0xDEADBEEF);
 	int bytes_gained = lTable->compress();
-	assert(lTable->code == 0xDEADBEEF);
 	totalEvict++;
-	ActiveSetEntryFree(victim->second);
+	delete victim->second;
+	victim->second = NULL;
 	active_set.erase(victim);
 	return bytes_gained;
 }
 
-int CBuffer::decompress(LevelTable* table) {
+int CBuffer::decompress(LevelTable *table) {
+	assert(table != NULL);
+
 	int loss = table->decompress();
 	int gain = this->add(table);
 	return gain - loss;
 }
 
-int CBuffer::add(LevelTable* table) {
+int CBuffer::add(LevelTable *table) {
+	assert(table != NULL);
 	assert(table->code == 0xDEADBEEF);
-	if (KConfigGetCompression() == 0)
-		return 0;
+
+	if (KConfigGetCompression() == 0) return 0;
 
 	int bytes_gained = 0;
+	// XXX: is next line really >=. Why not just >? (-sat)
 	if(active_set.size() >= this->num_entries) {
 		bytes_gained = evictFromBuffer();
 	}
@@ -217,9 +226,9 @@ int CBuffer::add(LevelTable* table) {
 	return bytes_gained;
 }
 
-void CBuffer::touch(LevelTable* table) {
-	if (KConfigGetCompression() == 0)
-		return;
+void CBuffer::touch(LevelTable *table) {
+	assert(table != NULL);
+	if (KConfigGetCompression() == 0) return;
 
 	std::map<LevelTable*, ActiveSetEntry*>::iterator it = active_set.find(table);
 	if (it == active_set.end()) {
