854992bfe52ce8f8e5aef31c8a3d4f88d33bbf4e
Sat Garcia <elsaturnino@gmail.com>
2013-08-19 16:45:20 -0700

    simple code cleanup

--- runtime/src/arg.cpp
+++ runtime/src/arg.cpp
@@ -14,12 +14,6 @@
 
 KremlinConfiguration kremlin_config;
 
-std::string __kremlin_output_filename;
-int __kremlin_level_to_log = -1;
-
-extern int __kremlin_debug;
-extern int __kremlin_debug_level;
-
 extern "C" int __main(int argc, char** argv);
 
 int parseOptionInt(char* option_str) {
@@ -47,32 +41,29 @@ char* parseOptionStr(char* option_str) {
 }
 
 
-void createOutputFilename() {
-	__kremlin_output_filename.clear(); // clear out old name
-	__kremlin_output_filename += "kremlin-L";
+void getCustomOutputFilename(std::string& filename) {
+	filename = "kremlin-L";
 	
 	// TODO: update to use to_string for C++11
 	std::stringstream ss;
 	ss << kremlin_config.getMinProfiledLevel();
-	__kremlin_output_filename += ss.str();
+	filename += ss.str();
 	ss.flush();
 
 	if(kremlin_config.getMaxProfiledLevel() != (kremlin_config.getMinProfiledLevel())) {
-		__kremlin_output_filename += "_";
+		filename += "_";
 		ss << kremlin_config.getMaxProfiledLevel();
-		__kremlin_output_filename += ss.str();
+		filename += ss.str();
 		ss.flush();
 	}
 	
-	__kremlin_output_filename += ".bin";
+	filename += ".bin";
 }
 
-void parseKremlinOptions(int argc, char* argv[], int& num_args, char**& real_args) {
+void parseKremlinOptions(int argc, char* argv[], unsigned& num_args, char**& real_args) {
 	std::vector<char*> true_args;
 
-	int parsed = 0;
-	int i;
-	for(i = 1; i < argc; ++i) {
+	for(unsigned i = 1; i < argc; ++i) {
 		fprintf(stderr,"checking %s\n",argv[i]);
 		char *str_start;
 
@@ -86,20 +77,6 @@ void parseKremlinOptions(int argc, char* argv[], int& num_args, char**& real_arg
 		}
 #endif
 
-#if 0
-		str_start = strstr(argv[i],"kremlin-ltl");
-		if(str_start) {
-			__kremlin_level_to_log = parseOptionInt(argv[i]);
-			setMinLevel(__kremlin_level_to_log);
-			setMaxLevel(__kremlin_level_to_log + 1);
-			continue;
-		}
-#endif
-		// TODO FIXME XXX: These option names are too generic: they need to be
-		// unique to Kremlin. These may come up in normal program usage
-		// and cause weird errors. This was exactly what happened for the
-		// streamcluster benchmark in rodinia since one of its inputs had the
-		// word "output" in it (which was previously a kremlin option). 
 		str_start = strstr(argv[i],"kremlin-disable-rsummary");
 		if(str_start) {
 			kremlin_config.disableRecursiveRegionSummarization();
@@ -184,34 +161,23 @@ void parseKremlinOptions(int argc, char* argv[], int& num_args, char**& real_arg
 			continue;
 		}
 		else {
-			//true_args[num_true_args] = strdup(argv[i]);
 			true_args.push_back(strdup(argv[i]));
-			//num_true_args++;
 		}
 	}
 
-	createOutputFilename(); // FIXME: this doesn't have any effect now
-
-	//true_args = realloc(true_args,num_true_args*sizeof(char*));
-	
 	real_args = new char*[true_args.size()];
 	for (unsigned i = 0; i < true_args.size(); ++i) {
 		real_args[i] = strdup(true_args[i]);
 	}
 	num_args = true_args.size();
-
-	//*num_args = num_true_args;
-	//*real_args = true_args;
 }
 
 // look for any kremlin specific inputs to the program
 int main(int argc, char* argv[]) {
-	int num_args = 0;;
+	unsigned num_args = 0;
 	char** real_args;
 
 	__kremlin_idbg = 0;
-
-	__kremlin_output_filename = "kremlin.bin";
 
 	parseKremlinOptions(argc,argv,num_args,real_args);
 
@@ -222,22 +188,16 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr,"[kremlin] Interactive debugging mode enabled.\n");
 	}
 
-	if(__kremlin_level_to_log == -1) {
-    	fprintf(stderr, "[kremlin] min level = %d, max level = %d\n", kremlin_config.getMinProfiledLevel(), kremlin_config.getMaxProfiledLevel());
-	}
-	else {
-    	fprintf(stderr, "[kremlin] logging only level %d\n", __kremlin_level_to_log);
-	}
+	fprintf(stderr, "[kremlin] min level = %d, max level = %d\n", kremlin_config.getMinProfiledLevel(), kremlin_config.getMaxProfiledLevel());
 
 	fprintf(stderr,"[kremlin] writing profiling data to: %s\n", kremlin_config.getProfileOutputFilename());
 	fprintf(stderr,"[kremlin] writing log to: %s\n", kremlin_config.getDebugOutputFilename());
 
-	int i;
 	char** start = &argv[argc - num_args-1];
 	start[0] = strdup(argv[0]);
 
 #if 0
-	for (i=0; i<=num_args; i++) {
+	for (unsigned i=0; i<=num_args; i++) {
 		fprintf(stderr, "arg %d: %s\n", i, start[i]);
 	}
 #endif
