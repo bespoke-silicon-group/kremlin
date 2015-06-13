09955939d81bf5a8c1242f3eb2b7c89224bb6e31
Sat Garcia <sat@sandiego.edu>
2015-06-12 19:02:30 -0700

    changed workaround to debug filename not set when initializing kremlin. still need a better way to handle this

--- runtime/src/Handlers.cpp
+++ runtime/src/Handlers.cpp
@@ -1174,7 +1174,7 @@ void KremlinProfiler::init() {
 		return;
     }
 	initialized = true;
-	DebugInit(false);
+	DebugInit();
 
     MSG(0, "Profile Level = (%d, %d), Index Size = %d\n", 
         getMinLevel(), getMaxLevel(), getArraySize());
